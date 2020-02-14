89547344338d82923a03d3afd44f5b3277a7bc3c
dtreskunov <denis.treskunov@gmail.com>
2020-02-13 20:07:14 -0800

    fix OPENBLAS_DIR: unbound variable
    travis/windows/build-openblas.sh: line 19: OPENBLAS_DIR: unbound variable

--- travis/windows/build-openblas.sh
+++ travis/windows/build-openblas.sh
@@ -3,15 +3,15 @@
 set -euo pipefail
 
 # OpenBLAS gets installed by Conan so we just need to find it
+cmake_property() {
+	cat "$1" | grep "set($2 " | sed -e 's/.*"\(.*\)").*/\1/'
+}
+
+OPENBLAS_DIR=$(cmake_property "${TRAVIS_BUILD_DIR}/conan_paths.cmake" CONAN_OPENBLAS_ROOT)
 (
 	echo "Starting OpenBLAS build at $(date)"
 	source "$(dirname "$0")/util.sh"
 
-	cmake_property() {
-		cat "$1" | grep "set($2 " | sed -e 's/.*"\(.*\)").*/\1/'
-	}
-
-	OPENBLAS_DIR=$(cmake_property "${TRAVIS_BUILD_DIR}/conan_paths.cmake" CONAN_OPENBLAS_ROOT)
 	echo "OpenBLAS is installed in ${OPENBLAS_DIR}"
 	find_files_with_ext .lib "$OPENBLAS_DIR"
 ) >&2
