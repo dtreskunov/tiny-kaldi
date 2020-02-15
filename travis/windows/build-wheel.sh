c35b96b84e8d3cb421d12196b3f41f12f84c147a
dtreskunov <denis.treskunov@gmail.com>
2020-02-14 19:18:26 -0800

    build for 3.5, 3.6, 3.7, and 3.8

---
+++ travis/windows/build-wheel.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+build_for_python_version() {
+	local version=$1
+	local two_digit_version=$(echo "$version" | sed -e 's:[^0-9]::g' | head -c 2)
+	local python_root="/c/Python${two_digit_version}"
+
+	echo "Building wheel for Python ${version}..."
+	choco install python3 --no-progress -y --version "$version"
+	"${python_root}/python" -m pip install pip wheel setuptools
+	"${python_root}/python" "${TRAVIS_BUILD_DIR}/python/setup.py" bdist_wheel
+}
+
+build_for_python_version "$1"
