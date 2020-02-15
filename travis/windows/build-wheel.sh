1a83c1ac4cc58890d5ccc5643231d9be785d15eb
Denis Treskunov <denis.treskunov@gmail.com>
2020-02-14 20:24:00 -0800

    fix "newer version of python3 is already installed"

--- travis/windows/build-wheel.sh
+++ travis/windows/build-wheel.sh
@@ -4,13 +4,16 @@ set -euo pipefail
 
 build_for_python_version() {
 	local version=$1
+	local dest=$2
 	local two_digit_version=$(echo "$version" | sed -e 's:[^0-9]::g' | head -c 2)
 	local python_root="/c/Python${two_digit_version}"
 
-	echo "Building wheel for Python ${version}..."
-	choco install python3 --no-progress -y --version "$version"
+	echo "Building wheel for Python ${version} to ${dest}..."
+	mkdir -p "${dest}"
+	choco install python3 --no-progress -y --force --version "$version"
 	"${python_root}/python" -m pip install pip wheel setuptools
-	"${python_root}/python" "${TRAVIS_BUILD_DIR}/python/setup.py" bdist_wheel
+	"${python_root}/python" "${TRAVIS_BUILD_DIR}/python/setup.py" bdist_wheel --dist-dir="$dest"
+	echo "Wheel built for Python ${version}!"
 }
 
 build_for_python_version "$1"
