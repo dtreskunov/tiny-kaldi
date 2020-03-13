f49fdc676add2001ca61b8f602be9bcda7eb9f0c
Denis Treskunov <denis.treskunov@gmail.com>
2020-03-13 13:14:24 -0700

    js code can now download and extract model

--- js/bindings.cc
+++ js/bindings.cc
@@ -1,11 +1,33 @@
 #include <emscripten/bind.h>
+#include "utils.h"
 #include "../src/kaldi_recognizer.h"
 #include "../src/model.h"
 #include "../src/spk_model.h"
 
 using namespace emscripten;
 
+struct ArchiveHelperWrapper : public wrapper<ArchiveHelper> {
+    EMSCRIPTEN_WRAPPER(ArchiveHelperWrapper);
+    void onsuccess() {
+        return call<void>("onsuccess");
+    }
+    void onerror(const std::string &what) {
+        return call<void>("onerror", what);
+    }
+};
+
 EMSCRIPTEN_BINDINGS(vosk) {
+    class_<ArchiveHelper>("ArchiveHelper")
+        .function("Extract", &ArchiveHelper::Extract)
+        .allow_subclass<ArchiveHelperWrapper>("ArchiveHelperWrapper")
+        .function("onsuccess", optional_override([](ArchiveHelper& self) {
+            return self.ArchiveHelper::onsuccess();
+        }))
+        .function("onerror", optional_override([](ArchiveHelper& self, const std::string &what) {
+            return self.ArchiveHelper::onerror(what);
+        }))
+        ;
+
     class_<Model>("Model")
         .constructor<const char *>()
         .function("SampleFrequency", &Model::SampleFrequency)
