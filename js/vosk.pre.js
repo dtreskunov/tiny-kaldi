89c2d85dc2d463c137aaeb3cebd081b28fbb0e9d
Denis Treskunov <denis.treskunov@gmail.com>
2020-03-14 18:42:13 -0700

    move utility functions from "example" to "dist"

--- js/vosk.pre.js
+++ js/vosk.pre.js
@@ -1,10 +1,1 @@
 // https://emscripten.org/docs/api_reference/module.html#Module.preRun
-Vosk.preRun = Vosk.preRun || [];
-Vosk.preRun.push(function() {
-    // plumb some libraries so they're accessible from user HTML
-    Vosk.FS = FS;
-    Vosk.MEMFS = MEMFS;
-    Vosk.NODEFS = NODEFS;
-    Vosk.IDBFS = IDBFS;
-    Vosk.WORKERFS = WORKERFS;
-});
